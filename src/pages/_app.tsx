d3fe0441c5ae0ae26839b7ec0c3f8121ae15ea8c
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2021-01-28 23:18:37 -0300

    Add SEO tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,44 @@
 import { ThemeProvider } from "styled-components";
-import GlobalStyle from "../styles/global";
-import { theme } from "../../db.json";
+import Head from "next/head";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+import GlobalStyle from "../styles/global";
+import { theme, bg, title, description } from "../../db.json";
+
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Head>
+        <title>Quiz | Guilherme Balog Gardino</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+
+        <meta name="description" content={description} />
+
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={bg} />
+
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:site_name"
+          content="Quiz | Guilherme Balog Gardino"
+        />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={`https://guilhermebalog.ga`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={bg} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={bg} />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
